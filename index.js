38ed46058f1b54e0c5d64c0b02d561c6bb7e4c3e
Benjamin Horns <benhorns@gmail.com>
2020-01-03 01:42:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -137,10 +137,12 @@ function moveDodger(e) {
     if (e.which === LEFT_ARROW){
       moveDodgerLeft()
     }
-    if (e.which === RIGHT_ARROW){
+    else if (e.which === RIGHT_ARROW){
       moveDodgerRight()
     }
-  
+    else {
+      
+    }
    
 }
 
